e58b7efe538e2d7bf0dd4345f05bb1c3b1184ac2
Robert Murdock <robert.f.murdock@accenture.com>
2019-03-02 22:07:11 -0500

    [patch] Adding assertnotequals to minassert.

--- minassert/src/commonMain/kotlin/com/zegreatrob/minassert/Assertions.kt
+++ minassert/src/commonMain/kotlin/com/zegreatrob/minassert/Assertions.kt
@@ -1,8 +1,11 @@
 package com.zegreatrob.minassert
 
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 fun <T> T?.assertIsEqualTo(expected: T, message: String? = null) = assertEquals(expected, this, message)
+
+fun <T> T?.assertIsNotEqualTo(expected: T, message: String? = null) = assertNotEquals(expected, this, message)
 
 fun <T> MutableList<T>.assertContains(item: T) = contains(item)
         .assertIsEqualTo(true, "${this.map { "$item" }} did not contain $item")
