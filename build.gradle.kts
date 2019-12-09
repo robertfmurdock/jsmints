bfac540d18139b2c12cdfa5b620a7c2cea1ace40
Robert Murdock <robert.f.murdock@accenture.com>
2019-12-09 10:27:16 -0500

    [patch] Updating a few dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,8 @@ buildscript {
 }
 
 plugins {
-    id("com.github.ben-manes.versions") version "0.21.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.13"
+    id("com.github.ben-manes.versions") version "0.27.0"
     id("de.gliderpilot.semantic-release") version "1.4.0"
 }
 
