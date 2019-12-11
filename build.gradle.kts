27b3c57fda46a29628fdea90e07a270a972f7d67
Robert Murdock <robert.f.murdock@accenture.com>
2019-12-11 09:30:57 -0500

    DO NOT PUSH, RELEASE TEST

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,6 +84,10 @@ subprojects {
     )
 
     tasks {
+        val publish by getting {
+            enabled = false
+        }
+
         val bintrayUpload by getting(BintrayUploadTask::class) {
 
             doFirst {
