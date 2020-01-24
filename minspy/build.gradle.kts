42975f473e2e809a91f3b58b3486dda1d30c2585
RoB Murdock <robert.f.murdock@gmail.com>
2020-01-24 14:23:41 -0500

    [patch] Adding minspy. Still very early, not sure I'm happy with it yet.

---
+++ minspy/build.gradle.kts
@@ -0,0 +1,62 @@
+import com.zegreatrob.testmints.build.BuildConstants
+import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
+
+plugins {
+    id("org.jetbrains.kotlin.multiplatform") version "1.3.61"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    targets {
+        jvm()
+        js {
+            //            nodejs {}
+        }
+        macosX64()
+        linuxX64()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-stdlib-common:${BuildConstants.kotlinVersion}")
+                implementation("org.jetbrains.kotlin:kotlin-test-common:${BuildConstants.kotlinVersion}")
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-test-junit:${BuildConstants.kotlinVersion}")
+            }
+        }
+
+        val nativeCommonMain by creating {
+            dependsOn(commonMain)
+        }
+
+        val macosX64Main by getting { dependsOn(nativeCommonMain) }
+
+        val linuxX64Main by getting { dependsOn(nativeCommonMain) }
+
+        getByName("jsMain") {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-stdlib-js:${BuildConstants.kotlinVersion}")
+                implementation("org.jetbrains.kotlin:kotlin-test-js")
+            }
+        }
+    }
+}
+
+tasks {
+    getByName<Kotlin2JsCompile>("compileKotlinJs") {
+        kotlinOptions.moduleKind = "umd"
+        kotlinOptions.sourceMap = true
+        kotlinOptions.sourceMapEmbedSources = "always"
+    }
+}
+
+
+
