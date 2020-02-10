1cf99a24b162d85fdd5efaa18239691ee40d211e
RoB Murdock <robert.f.murdock@gmail.com>
2020-02-10 13:09:09 -0500

    [patch] Adding new line before natural assertion result.

--- minassert/src/commonMain/kotlin/com/zegreatrob/minassert/Assertions.kt
+++ minassert/src/commonMain/kotlin/com/zegreatrob/minassert/Assertions.kt
@@ -7,7 +7,7 @@ import kotlin.test.assertNotEquals
 fun <T> T?.assertIsEqualTo(expected: T, message: String? = null) = assertEquals(expected, this, message.appendDiff(expected, this))
 
 private fun <T> String?.appendDiff(expected: T, t1: T?): String {
-    return "${this ?: ""}\n${stringDiff(expected.toString(), t1.toString())}"
+    return "${this ?: ""}\n${stringDiff(expected.toString(), t1.toString())}\n"
 }
 
 fun <T> T?.assertIsNotEqualTo(expected: T, message: String? = null) = assertNotEquals(expected, this, message)
