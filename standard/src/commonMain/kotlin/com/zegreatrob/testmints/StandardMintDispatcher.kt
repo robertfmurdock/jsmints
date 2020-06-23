4983689776714d5460f59564d7bd28dd3836402f
RoB Murdock <robert.f.murdock@gmail.com>
2020-06-23 10:38:39 -0400

    [patch] Some minor refactoring

--- standard/src/commonMain/kotlin/com/zegreatrob/testmints/StandardMintDispatcher.kt
+++ standard/src/commonMain/kotlin/com/zegreatrob/testmints/StandardMintDispatcher.kt
@@ -1,6 +1,5 @@
 package com.zegreatrob.testmints
 
-import com.zegreatrob.testmints.report.MintReporter
 import com.zegreatrob.testmints.report.ReporterProvider
 
 
@@ -16,52 +15,15 @@ interface StandardMintDispatcher : ReporterProvider {
             { context() }, reporter, additionalSetupActions, { it(Unit) }
     )
 
-    fun <SC : Any> testTemplate(sharedSetup: () -> SC, sharedTeardown: (SC) -> Unit) = TestTemplate<SC>(
-            reporter
-    ) {
-        val sc = sharedSetup()
-        it(sc)
-        sharedTeardown(sc)
-    }
-
-    fun testTemplateSimple(wrapper: (() -> Unit) -> Unit): TestTemplate<Unit> = TestTemplate(reporter) {
-        wrapper { it(Unit) }
-    }
-
     fun <SC : Any> testTemplate(wrapper: ((SC) -> Unit) -> Unit): TestTemplate<SC> = TestTemplate(reporter, wrapper)
 
-}
-
-class TestTemplate<SC : Any>(
-        private val reporter: MintReporter,
-        private val wrapper: ((SC) -> Unit) -> Unit
-) {
-    operator fun <C : Any> invoke(context: C, additionalSetupActions: C.() -> Unit = {}) =
-            Setup({ context }, reporter, additionalSetupActions, wrapper)
-
-    operator fun <C : Any> invoke(contextProvider: (SC) -> C, additionalSetupActions: C.() -> Unit = {}) =
-            Setup(contextProvider, reporter, additionalSetupActions, wrapper)
-
-    fun extend(sharedSetup: () -> Unit = {}, sharedTeardown: () -> Unit = {}) = TestTemplate<SC>(
-            reporter = reporter,
-            wrapper = { test ->
-                wrapper {
-                    sharedSetup()
-                    test(it)
-                    sharedTeardown()
-                }
-            }
-    )
-
-    fun <SC2 : Any> extend(wrapper: (SC, (SC2) -> Unit) -> Unit) = TestTemplate<SC2>(reporter) { test ->
-        this.wrapper { sc1 -> wrapper(sc1, test) }
+    fun <SC : Any> testTemplate(sharedSetup: () -> SC, sharedTeardown: (SC) -> Unit) = testTemplate<SC> { test ->
+        sharedSetup()
+                .also(test)
+                .also(sharedTeardown)
     }
 
-    fun <SC2 : Any> extend(sharedSetup: (SC) -> SC2, sharedTeardown: (SC2) -> Unit = {}) = extend<SC2> { sc1, test ->
-        val sc2 = sharedSetup(sc1)
-        test(sc2)
-        sharedTeardown(sc2)
-    }
+    fun testTemplateSimple(wrapper: (() -> Unit) -> Unit): TestTemplate<Unit> = testTemplate { wrapper { it(Unit) } }
 
 }
 
---
+++ standard/src/commonMain/kotlin/com/zegreatrob/testmints/TestTemplate.kt
@@ -0,0 +1,34 @@
+package com.zegreatrob.testmints
+
+import com.zegreatrob.testmints.report.MintReporter
+
+class TestTemplate<SC : Any>(private val reporter: MintReporter, private val wrapper: ((SC) -> Unit) -> Unit) {
+
+    operator fun <C : Any> invoke(context: C, additionalSetupActions: C.() -> Unit = {}) =
+            Setup({ context }, reporter, additionalSetupActions, wrapper)
+
+    operator fun <C : Any> invoke(contextProvider: (SC) -> C, additionalSetupActions: C.() -> Unit = {}) =
+            Setup(contextProvider, reporter, additionalSetupActions, wrapper)
+
+    fun <SC2 : Any> extend(wrapper: (SC, (SC2) -> Unit) -> Unit) = TestTemplate<SC2>(reporter) { test ->
+        this.wrapper { sc1 -> wrapper(sc1, test) }
+    }
+
+    fun <SC2 : Any> extend(sharedSetup: (SC) -> SC2, sharedTeardown: (SC2) -> Unit = {}) = extend<SC2> { sc1, test ->
+        val sc2 = sharedSetup(sc1)
+        test(sc2)
+        sharedTeardown(sc2)
+    }
+
+    fun extend(sharedSetup: () -> Unit = {}, sharedTeardown: () -> Unit = {}) = TestTemplate<SC>(
+            reporter = reporter,
+            wrapper = { test ->
+                wrapper {
+                    sharedSetup()
+                    test(it)
+                    sharedTeardown()
+                }
+            }
+    )
+
+}
