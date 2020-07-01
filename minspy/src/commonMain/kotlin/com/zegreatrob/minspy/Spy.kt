48cc640e4bdce3acad16489b8ab35ed5e11d2c83
RoB Murdock <robert.f.murdock@gmail.com>
2020-07-01 15:39:57 -0400

    [patch] Pulling a few things into their own files.

--- minspy/src/commonMain/kotlin/com/zegreatrob/minspy/Spy.kt
+++ minspy/src/commonMain/kotlin/com/zegreatrob/minspy/Spy.kt
@@ -1,7 +1,5 @@
 package com.zegreatrob.minspy
 
-import kotlin.test.fail
-
 interface Spy<I, O> {
     val spyReceivedValues: MutableList<I>
 
@@ -26,36 +24,4 @@ interface Spy<I, O> {
     }
 
     fun cancel(): Nothing = throw NotImplementedError("Will not implement unused collaborator")
-}
-
-fun Spy<Unit, Unit>.spyFunction() {
-    spyReceivedValues.add(Unit)
-}
-
-fun <O> Spy<Unit, O>.spyFunction() = spyFunction(Unit)
-
-fun <I> Spy<I, Unit>.spyFunction(input: I) {
-    spyReceivedValues.add(input)
-}
-
-fun <I, O> Spy<I, O>.spyFunction(input: I): O = spyReturnWhenGivenValues.fixedGetOrElse(input) {
-    safePop(input)
-}.also { spyReceivedValues.add(input) }
-
-private fun <I, O> Spy<I, O>.safePop(input: I): O = if (spyReturnValues.size > 0)
-    spyReturnValues[0].also { spyReturnValues.removeAt(0) }
-else
-    fail("No values remaining given input $input")
-
-
-private fun <K, V> Map<K, V>.fixedGetOrElse(input: K, function: () -> V) = if (containsKey(input)) {
-    getValue(input)
-} else {
-    function()
-}
-
-class SpyData<I, O> : Spy<I, O> {
-    override val spyReturnWhenGivenValues = mutableMapOf<I, O>()
-    override val spyReceivedValues = mutableListOf<I>()
-    override val spyReturnValues = mutableListOf<O>()
 }
