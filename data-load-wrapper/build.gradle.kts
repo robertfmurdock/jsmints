73b482178801087f9dd4a9911d23809018b00872
RoB Murdock <robert.f.murdock@gmail.com>
2020-07-30 15:49:03 -0400

    [patch] Adding data load wrapper module.

---
+++ data-load-wrapper/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")  version "1.3.72"
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://kotlin.bintray.com/kotlinx") }
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlin-js-wrappers") }
+}
+
+kotlin {
+    targets {
+        js {
+            nodejs {}
+            useCommonJs()
+        }
+    }
+
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(project(":minreact"))
+                implementation("org.jetbrains:kotlin-react:16.13.1-pre.110-kotlin-1.3.72")
+                implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.110-kotlin-1.3.72")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+            }
+        }
+
+        val jsTest by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-test-common")
+                implementation("org.jetbrains.kotlin:kotlin-test-js")
+            }
+        }
+    }
+}
