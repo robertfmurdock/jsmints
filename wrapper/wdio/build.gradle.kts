72c35116ae1544c30c0b6770f42f610bc8e4accc
RoB Murdock <robert.f.murdock@gmail.com>
2020-08-13 20:21:09 -0400

    [patch] Fixing it so wdio can publish.

--- wrapper/wdio/build.gradle.kts
+++ wrapper/wdio/build.gradle.kts
@@ -1,21 +1,26 @@
 plugins {
-    kotlin("js") version "1.3.72"
+    kotlin("multiplatform") version "1.3.72"
 }
 
 kotlin {
-    target {
-        nodejs {}
-        useCommonJs()
+    targets {
+        js {
+            nodejs {}
+            useCommonJs()
+        }
     }
 
     sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.8")
+                implementation("com.soywiz.korlibs.klock:klock:1.10.6")
+                implementation("io.github.microutils:kotlin-logging-js:1.8.3")
+                implementation(npm("@wdio/cli"))
+            }
+        }
     }
+
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.8")
-    implementation("com.soywiz.korlibs.klock:klock:1.10.6")
-    implementation("io.github.microutils:kotlin-logging-js:1.8.3")
-    implementation(npm("@wdio/cli"))
-}
