694aa250d4c8c60908e085baf12b031bd0fff0b5
RoB Murdock <robert.f.murdock@gmail.com>
2020-08-18 11:51:50 -0400

    [patch] Upgrading klock version.

--- wdio/build.gradle.kts
+++ wdio/build.gradle.kts
@@ -15,7 +15,7 @@ kotlin {
             dependencies {
                 implementation(kotlin("stdlib"))
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.9")
-                implementation("com.soywiz.korlibs.klock:klock:1.10.6")
+                implementation("com.soywiz.korlibs.klock:klock:1.12.0")
                 implementation("io.github.microutils:kotlin-logging-js:1.8.3")
                 implementation(npm("@wdio/cli", "6.4.0"))
             }
