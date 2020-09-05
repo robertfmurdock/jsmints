0f38011762e1073aaabbb6813bb3c20b055e851a
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2020-09-05 14:07:01 +0000

    [patch] Automatic dependency update.

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ allprojects {
 
 plugins {
     id("se.patrikerdes.use-latest-versions") version "0.2.14"
-    id("com.github.ben-manes.versions") version "0.29.0"
+    id("com.github.ben-manes.versions") version "0.30.0"
     id("de.gliderpilot.semantic-release") version "1.4.0"
 }
 
