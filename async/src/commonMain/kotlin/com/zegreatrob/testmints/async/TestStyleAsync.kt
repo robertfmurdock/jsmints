0496e2365e7bc1decad89e641d2a39fc0aeed92f
RoB Murdock <robert.f.murdock@gmail.com>
2020-09-11 08:38:28 -0400

    [patch] deprecating testAsync

--- async/src/commonMain/kotlin/com/zegreatrob/testmints/async/TestStyleAsync.kt
+++ async/src/commonMain/kotlin/com/zegreatrob/testmints/async/TestStyleAsync.kt
@@ -3,6 +3,7 @@ package com.zegreatrob.testmints.async
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 
+@Deprecated("testAsync is deprecated, transition to using asyncSetup chaining instead.")
 expect fun <T> testAsync(block: suspend CoroutineScope.() -> T)
 
 expect fun finalTransform(it: () -> Deferred<Unit>)
