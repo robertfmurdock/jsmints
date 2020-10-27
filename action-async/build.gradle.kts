599acfcc90efdc68ce9422fc2fcb0868e9a53eda
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2020-10-27 14:08:01 +0000

    [patch] Automatic dependency update.

--- action-async/build.gradle.kts
+++ action-async/build.gradle.kts
@@ -19,7 +19,7 @@ kotlin {
             dependencies {
                 implementation(project(":action"))
                 implementation("org.jetbrains.kotlin:kotlin-stdlib-common:1.4.10")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.0-M1")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.0")
             }
         }
         getByName("commonTest") {
