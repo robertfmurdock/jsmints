5799918f299ab8a1e47f4674d306c15ad5eb5a9c
RoB Murdock <robert.f.murdock@gmail.com>
2021-03-05 18:46:30 -0500

    [patch] updating kotlin logging

--- wdio/build.gradle.kts
+++ wdio/build.gradle.kts
@@ -17,7 +17,7 @@ kotlin {
                 implementation(kotlin("stdlib"))
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.4.2")
                 implementation("com.soywiz.korlibs.klock:klock:1.12.0")
-                implementation("io.github.microutils:kotlin-logging:2.0.4")
+                implementation("io.github.microutils:kotlin-logging:2.0.6")
                 implementation(npm("@wdio/cli", "7.0.8"))
             }
         }
