4b228ab118806b5983ee8c01699e55d4f365f8c9
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2021-04-27 14:10:55 +0000

    [patch] Automatic dependency update.

--- action-async/build.gradle.kts
+++ action-async/build.gradle.kts
@@ -45,7 +45,7 @@ kotlin {
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("reflect", "1.5.0"))
-                implementation("org.slf4j:slf4j-simple:1.7.5")
+                implementation("org.slf4j:slf4j-simple:2.0.0-alpha1")
                 implementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
                 implementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
             }
