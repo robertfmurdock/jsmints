9af37ca764c17221d562ea9918b13841eadd2b7b
RoB Murdock <robert.f.murdock@gmail.com>
2021-04-30 18:02:36 -0400

    [patch] updating klock for wdio

--- wdio/build.gradle.kts
+++ wdio/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
             dependencies {
                 implementation(kotlin("stdlib"))
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-                implementation("com.soywiz.korlibs.klock:klock:2.0.7")
+                implementation("com.soywiz.korlibs.klock:klock:2.1.0")
                 implementation("io.github.microutils:kotlin-logging:2.0.6")
                 implementation(npm("@wdio/cli", "7.0.8"))
             }
