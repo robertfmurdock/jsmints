ea7005eb5e46ffc0f388a9fbb5b944f3f5d947a1
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2021-06-02 14:11:03 +0000

    [patch] Automatic dependency update.

--- wdio/build.gradle.kts
+++ wdio/build.gradle.kts
@@ -19,7 +19,7 @@ kotlin {
                 implementation(kotlin("stdlib"))
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
                 implementation("com.soywiz.korlibs.klock:klock:2.1.0")
-                implementation("io.github.microutils:kotlin-logging:2.0.6")
+                implementation("io.github.microutils:kotlin-logging:2.0.8")
                 implementation(npm("@wdio/cli", "7.0.8"))
             }
         }
