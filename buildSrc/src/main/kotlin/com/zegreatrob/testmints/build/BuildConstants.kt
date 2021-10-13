a1e415d4a454e0dab0369ab90eaaa840e7a12a14
RoB Murdock <robert.f.murdock@gmail.com>
2021-10-13 12:02:49 -0400

    kotlin update

--- buildSrc/src/main/kotlin/com/zegreatrob/testmints/build/BuildConstants.kt
+++ buildSrc/src/main/kotlin/com/zegreatrob/testmints/build/BuildConstants.kt
@@ -1,6 +1,6 @@
 package com.zegreatrob.testmints.build
 
 object BuildConstants {
-    const val kotlinVersion = "1.5.30"
-    const val coroutinesVersion = "1.5.1"
+    const val kotlinVersion = "1.5.31"
+    const val coroutinesVersion = "1.5.2"
 }
