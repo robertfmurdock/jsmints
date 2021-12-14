cb6d15a61d86c7482efb8e74bc61446614839daf
RoB Murdock <robert.f.murdock@gmail.com>
2021-12-14 13:57:33 -0500

    [patch] updating kotlin

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -7,13 +7,13 @@ repositories {
 }
 
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.6.0")
+    id("org.jetbrains.kotlin.jvm").version("1.6.10")
     `kotlin-dsl`
     id("com.github.ben-manes.versions") version("0.39.0")
     id("se.patrikerdes.use-latest-versions") version("0.2.18")
 }
 
-val kotlinVersion = "1.6.0"
+val kotlinVersion = "1.6.10"
 
 dependencies {
     implementation(kotlin("stdlib", kotlinVersion))
--- plugins/src/main/kotlin/com/zegreatrob/testmints/plugins/BuildConstants.kt
+++ plugins/src/main/kotlin/com/zegreatrob/testmints/plugins/BuildConstants.kt
@@ -1,6 +1,6 @@
 package com.zegreatrob.testmints.plugins
 
 object BuildConstants {
-    const val kotlinVersion = "1.6.0"
+    const val kotlinVersion = "1.6.10"
     const val coroutinesVersion = "1.5.2"
 }
