bd6bf69db1aa7afdc8bfa6871ce2b1a399146e22
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2022-01-17 14:14:49 +0000

    [patch] Automatic dependency update.

--- kotest-example/build.gradle.kts
+++ kotest-example/build.gradle.kts
@@ -3,7 +3,7 @@ import com.zegreatrob.testmints.plugins.BuildConstants
 plugins {
     id("com.zegreatrob.testmints.plugins.versioning")
     id("com.zegreatrob.testmints.plugins.platforms")
-    id("io.kotest.multiplatform") version "5.0.3"
+    id("io.kotest.multiplatform") version "5.1.0"
 }
 
 kotlin {
@@ -19,7 +19,7 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation("org.jetbrains.kotlin:kotlin-test:${BuildConstants.kotlinVersion}")
-                implementation("io.kotest:kotest-framework-engine:5.0.3")
+                implementation("io.kotest:kotest-framework-engine:5.1.0")
             }
         }
 
@@ -37,7 +37,7 @@ kotlin {
 
         val jvmTest by getting {
             dependencies {
-                implementation("io.kotest:kotest-runner-junit5-jvm:5.0.3")
+                implementation("io.kotest:kotest-runner-junit5-jvm:5.1.0")
             }
         }
     }
