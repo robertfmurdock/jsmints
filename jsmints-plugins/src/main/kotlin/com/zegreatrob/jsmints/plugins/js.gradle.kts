f036d377b2b40d65e73f2f9830dcd7067f470630
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2022-01-21 15:07:25 +0000

    [patch] Automatic dependency update.

--- jsmints-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/js.gradle.kts
+++ jsmints-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/js.gradle.kts
@@ -16,5 +16,5 @@ kotlin {
 
 dependencies {
     "commonMainImplementation"(platform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:0.0.1-pre.290-kotlin-1.6.10"))
-    "commonMainImplementation"(platform("com.zegreatrob.testmints:testmints-bom:5.6.6"))
+    "commonMainImplementation"(platform("com.zegreatrob.testmints:testmints-bom:6.0.1"))
 }
