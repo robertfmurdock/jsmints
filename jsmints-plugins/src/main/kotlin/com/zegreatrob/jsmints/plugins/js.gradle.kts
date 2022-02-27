b8204119f5fe55b4e94e7cd03b7d8b5221fc525b
RoB Murdock <robert.f.murdock@gmail.com>
2022-02-26 20:48:32 -0500

    manually updating kotlin wrappers

--- jsmints-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/js.gradle.kts
+++ jsmints-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/js.gradle.kts
@@ -15,6 +15,6 @@ kotlin {
 }
 
 dependencies {
-    "commonMainImplementation"(platform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:0.0.1-pre.307-kotlin-1.6.10"))
-    "commonMainImplementation"(platform("com.zegreatrob.testmints:testmints-bom:6.0.3"))
+    "commonMainImplementation"(platform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:0.0.1-pre.308-kotlin-1.6.10"))
+    "commonMainImplementation"(platform("com.zegreatrob.testmints:testmints-bom:6.0.4"))
 }
