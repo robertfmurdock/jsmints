ed6fc953e9b72db3dee7491d94e213aa1f4bf226
RoB Murdock <robert.f.murdock@gmail.com>
2022-05-04 23:03:49 -0400

    [patch] doing some cache adjustments

--- jsmints-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/js.gradle.kts
+++ jsmints-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/js.gradle.kts
@@ -1,5 +1,7 @@
 package com.zegreatrob.jsmints.plugins
 
+import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest
+
 plugins {
     kotlin("js")
     id("com.zegreatrob.jsmints.plugins.reports")
@@ -18,4 +20,14 @@ dependencies {
     implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.1"))
     implementation(platform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:0.0.1-pre.333"))
     implementation(platform("com.zegreatrob.testmints:testmints-bom:7.3.3"))
+}
+
+tasks.withType(KotlinJsTest::class).configureEach {
+    outputs.cacheIf { true }
+}
+tasks.withType(org.gradle.api.tasks.bundling.Jar::class).configureEach {
+    outputs.cacheIf { true }
+}
+tasks.withType(org.gradle.jvm.tasks.Jar::class).configureEach {
+    outputs.cacheIf { true }
 }
