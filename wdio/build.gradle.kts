250ba87e851927cff2dd779924442f0de43cad5e
robertfmurdock <robertfmurdock@users.noreply.github.com>
2022-05-21 15:11:27 +0000

    [patch] Gradle Libraries Update

--- wdio/build.gradle.kts
+++ wdio/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
                 implementation(kotlin("stdlib"))
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
                 implementation("com.soywiz.korlibs.klock:klock:2.7.0")
-                implementation("io.github.microutils:kotlin-logging:2.1.21")
+                implementation("io.github.microutils:kotlin-logging:2.1.23")
                 implementation(npm("@wdio/cli", "7.10.0"))
                 implementation(npm("@wdio/local-runner", "7.10.0"))
             }
