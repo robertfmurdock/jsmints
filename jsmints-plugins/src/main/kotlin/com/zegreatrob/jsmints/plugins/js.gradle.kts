d07c05980be523baa9b3d4de3db6916b8944c092
robertfmurdock <robertfmurdock@users.noreply.github.com>
2022-05-25 15:13:03 +0000

    [patch] Gradle Libraries Update

--- jsmints-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/js.gradle.kts
+++ jsmints-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/js.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
 
 dependencies {
     "commonMainImplementation"(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.1"))
-    "commonMainImplementation"(platform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.339"))
+    "commonMainImplementation"(platform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.340"))
     "commonMainImplementation"(platform("com.zegreatrob.testmints:testmints-bom:7.4.8"))
 }
 
