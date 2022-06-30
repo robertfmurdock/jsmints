30c04e18d677af3f9711b3a6fa0c6ea0e920e9fe
RoB Murdock <robert.f.murdock@gmail.com>
2022-06-29 21:42:41 -0400

    [patch] sources and code

--- js-package-plugin/build.gradle.kts
+++ js-package-plugin/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     `kotlin-dsl`
     id("com.zegreatrob.jsmints.plugins.versioning")
     id("com.zegreatrob.jsmints.plugins.publish")
+    id("com.gradle.plugin-publish") version "1.0.0"
 }
 
 val kotlinVersion = "1.7.0"
@@ -18,3 +19,11 @@ dependencies {
     implementation(kotlin("gradle-plugin", kotlinVersion))
     api("com.fasterxml.jackson.core:jackson-databind:2.13.3")
 }
+
+//pluginBundle {
+//    website = REPO_URL
+//    vcsUrl = REPO_URL
+//
+//    pluginTags = KfcPlugin.values()
+//        .associate { it.pluginName to it.tags }
+//}
