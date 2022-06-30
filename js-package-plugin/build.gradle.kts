e859790971be3e337ea4e02151c15288351103cd
RoB Murdock <robert.f.murdock@gmail.com>
2022-06-29 21:51:42 -0400

    [patch] removing redundant publish plugin from plugin

--- js-package-plugin/build.gradle.kts
+++ js-package-plugin/build.gradle.kts
@@ -1,3 +1,7 @@
+
+import java.nio.charset.Charset
+import java.util.*
+
 repositories {
     maven { url = uri("https://plugins.gradle.org/m2/") }
     mavenCentral()
@@ -8,11 +12,12 @@ plugins {
     id("org.jetbrains.kotlin.jvm")
     `kotlin-dsl`
     id("com.zegreatrob.jsmints.plugins.versioning")
-    id("com.zegreatrob.jsmints.plugins.publish")
     id("com.gradle.plugin-publish") version "1.0.0"
+    signing
 }
 
 val kotlinVersion = "1.7.0"
+group = "com.zegreatrob.jsmints"
 
 dependencies {
     implementation(kotlin("stdlib", kotlinVersion))
@@ -20,10 +25,61 @@ dependencies {
     api("com.fasterxml.jackson.core:jackson-databind:2.13.3")
 }
 
-//pluginBundle {
-//    website = REPO_URL
-//    vcsUrl = REPO_URL
-//
-//    pluginTags = KfcPlugin.values()
-//        .associate { it.pluginName to it.tags }
-//}
+afterEvaluate {
+    publishing.publications.withType<MavenPublication>().forEach {
+        with(it) {
+            val scmUrl = "https://github.com/robertfmurdock/jsmints"
+
+            pom.name.set(project.name)
+            pom.description.set(project.name)
+            pom.url.set(scmUrl)
+
+            pom.licenses {
+                license {
+                    name.set("MIT License")
+                    url.set(scmUrl)
+                    distribution.set("repo")
+                }
+            }
+            pom.developers {
+                developer {
+                    id.set("robertfmurdock")
+                    name.set("Rob Murdock")
+                    email.set("robert.f.murdock@gmail.com")
+                }
+            }
+            pom.scm {
+                url.set(scmUrl)
+                connection.set("git@github.com:robertfmurdock/jsmints.git")
+                developerConnection.set("git@github.com:robertfmurdock/jsmints.git")
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+
+    if (signingKey != null) {
+        val decodedKey = Base64.getDecoder().decode(signingKey).toString(Charset.defaultCharset())
+        useInMemoryPgpKeys(
+            decodedKey,
+            signingPassword
+        )
+    }
+    sign(publishing.publications)
+}
+
+tasks {
+    publish { finalizedBy("::closeAndReleaseSonatypeStagingRepository") }
+    val javadocJar by creating(Jar::class) {
+        archiveClassifier.set("javadoc")
+        from("${rootDir.absolutePath}/javadocs")
+    }
+    publishing.publications {
+        withType<MavenPublication> { artifact(javadocJar) }
+    }
+}
+
+fun PublicationContainer.jvmPublication(): NamedDomainObjectSet<Publication> = matching { it.name == "jvm" }
