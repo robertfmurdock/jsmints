1f46451e51d76bad934e59e2c2dca27d3427bdd5
RoB Murdock <robert.f.murdock@gmail.com>
2022-06-30 00:05:14 -0400

    [patch] removing extraneous plugin content

--- js-package-plugin/src/main/kotlin/com/zegreatrob/jsmints/plugins/jspackage.gradle.kts
+++ js-package-plugin/src/main/kotlin/com/zegreatrob/jsmints/plugins/jspackage.gradle.kts
@@ -6,13 +6,6 @@ plugins {
 
 val toolsExtension = project.extensions.create("jstools", JsToolsExtension::class, loadPackageJson())
 
-kotlin {
-    js {
-        useCommonJs()
-        binaries.executable()
-    }
-}
-
 dependencies {
     toolsExtension.packageJson.dependencies()?.forEach {
         implementation(npm(it.first, it.second.asText()))
@@ -20,10 +13,4 @@ dependencies {
     toolsExtension.packageJson.devDependencies()?.forEach {
         testImplementation(npm(it.first, it.second.asText()))
     }
-    implementation(enforcedPlatform("com.zegreatrob.testmints:testmints-bom:8.0.3"))
-    implementation(enforcedPlatform("com.zegreatrob.jsmints:jsmints-bom:1.5.7"))
-    implementation(enforcedPlatform("io.ktor:ktor-bom:2.0.3"))
-    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.348"))
-    implementation(enforcedPlatform("org.jetbrains.kotlinx:kotlinx-serialization-bom:1.3.3"))
-    implementation(enforcedPlatform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.3"))
 }
