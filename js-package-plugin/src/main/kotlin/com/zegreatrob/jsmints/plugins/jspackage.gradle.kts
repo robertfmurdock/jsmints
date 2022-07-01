c588f18a5ec777d6cdd6e303b363b41069f5c461
RoB Murdock <robert.f.murdock@gmail.com>
2022-06-30 22:09:23 -0400

    [patch] adding kotlin plugin overrides for specific libraries

--- js-package-plugin/src/main/kotlin/com/zegreatrob/jsmints/plugins/jspackage.gradle.kts
+++ js-package-plugin/src/main/kotlin/com/zegreatrob/jsmints/plugins/jspackage.gradle.kts
@@ -1,5 +1,7 @@
 package com.zegreatrob.jsmints.plugins
 
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
+
 plugins {
     kotlin("js")
 }
@@ -12,5 +14,14 @@ dependencies {
     }
     jspackage.devDependencies()?.forEach {
         testImplementation(npm(it.first, it.second.asText()))
+    }
+}
+
+NodeJsRootPlugin.apply(rootProject).apply {
+    val libs = jspackage.dependencies()?.toMap()
+    if (libs != null) {
+        libs["webpack"]?.asText()?.let { versions.webpack.version = it }
+        libs["webpack-cli"]?.asText()?.let { versions.webpackCli.version = it }
+        libs["karma"]?.asText()?.let { versions.karma.version = it }
     }
 }
--- js-package-plugin/src/main/kotlin/com/zegreatrob/jsmints/plugins/ncu.gradle.kts
+++ js-package-plugin/src/main/kotlin/com/zegreatrob/jsmints/plugins/ncu.gradle.kts
@@ -21,7 +21,6 @@ tasks {
             }
         }
     }
-
 }
 
 dependencies {
