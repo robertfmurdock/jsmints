886500bde37c45cb5ed17cd1cdac62e857e4d82a
RoB Murdock <robert.f.murdock@gmail.com>
2022-06-30 21:09:57 -0400

    [patch] adding an ncu plugin

---
+++ js-package-plugin/src/main/kotlin/com/zegreatrob/jsmints/plugins/ncu.gradle.kts
@@ -0,0 +1,27 @@
+package com.zegreatrob.jsmints.plugins
+
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsExec
+
+plugins {
+    id("com.zegreatrob.jsmints.plugins.jspackage")
+}
+
+tasks {
+    kotlin.js().compilations.named("test").configure {
+        val packageJson = "${project.projectDir.absolutePath}/package.json"
+        NodeJsExec.create(this, "ncuUpgrade") {
+            val nodeCommand = "ncu"
+            val nodeCommandBin = "${project.nodeModulesDir}/.bin/$nodeCommand"
+            nodeArgs.addAll(
+                "$nodeCommandBin -u --packageFile $packageJson --configFilePath $rootDir/.ncurc.json".split(" ")
+            )
+        }
+    }
+
+}
+
+dependencies {
+    testImplementation(npm("npm-check-updates", "^15.0.0"))
+}
+
+val Project.nodeModulesDir get() = rootProject.buildDir.resolve("js/node_modules")
