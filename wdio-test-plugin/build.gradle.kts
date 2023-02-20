d080f3cec6fd6028e880f4e4d8d590f9c0cdcf59
RoB Murdock <robert.f.murdock@gmail.com>
2023-02-20 17:30:24 -0500

    [patch] disabling inner version number in attempt to get core build green again

--- wdio-test-plugin/build.gradle.kts
+++ wdio-test-plugin/build.gradle.kts
@@ -1,13 +1,13 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
-import de.gliderpilot.gradle.semanticrelease.GithubRepo
-import de.gliderpilot.gradle.semanticrelease.SemanticReleaseChangeLogService
-import de.gliderpilot.gradle.semanticrelease.SemanticReleaseInitialStateService
-import de.gliderpilot.gradle.semanticrelease.SemanticReleaseStrategy
-import org.ajoberstar.gradle.git.release.semver.ChangeScope
+// import de.gliderpilot.gradle.semanticrelease.GithubRepo
+// import de.gliderpilot.gradle.semanticrelease.SemanticReleaseChangeLogService
+// import de.gliderpilot.gradle.semanticrelease.SemanticReleaseInitialStateService
+// import de.gliderpilot.gradle.semanticrelease.SemanticReleaseStrategy
+// import org.ajoberstar.gradle.git.release.semver.ChangeScope
 
 plugins {
-    alias(libs.plugins.de.gliderpilot.semantic.release)
+    // alias(libs.plugins.de.gliderpilot.semantic.release)
     alias(libs.plugins.nl.littlerobots.version.catalog.update)
     alias(libs.plugins.io.github.gradle.nexus.publish.plugin)
     `maven-publish`
@@ -17,30 +17,30 @@ plugins {
     id("com.zegreatrob.jsmints.plugins.versioning")
 }
 
-semanticRelease {
-    changeLog(closureOf<SemanticReleaseChangeLogService> {
-        changeScope = KotlinClosure1<org.ajoberstar.grgit.Commit, ChangeScope>({
-            val version = extractVersion()
-            when (version?.uppercase()) {
-                "MAJOR" -> ChangeScope.MAJOR
-                "MINOR" -> ChangeScope.MINOR
-                "PATCH" -> ChangeScope.PATCH
-                else -> null
-            }
-        })
-    })
-}
-
-release {
-    versionStrategy(
-        semanticRelease.releaseStrategy.copyWith(
-            mapOf(
-                "selector" to de.gliderpilot.gradle.semanticrelease.SemanticReleaseStrategySelector { true },
-                "createTag" to false
-            )
-        )
-    )
-}
+// semanticRelease {
+//     changeLog(closureOf<SemanticReleaseChangeLogService> {
+//         changeScope = KotlinClosure1<org.ajoberstar.grgit.Commit, ChangeScope>({
+//             val version = extractVersion()
+//             when (version?.uppercase()) {
+//                 "MAJOR" -> ChangeScope.MAJOR
+//                 "MINOR" -> ChangeScope.MINOR
+//                 "PATCH" -> ChangeScope.PATCH
+//                 else -> null
+//             }
+//         })
+//     })
+// }
+//
+// release {
+//     versionStrategy(
+//         semanticRelease.releaseStrategy.copyWith(
+//             mapOf(
+//                 "selector" to de.gliderpilot.gradle.semanticrelease.SemanticReleaseStrategySelector { true },
+//                 "createTag" to false
+//             )
+//         )
+//     )
+// }
 
 nexusPublishing {
     repositories {
@@ -61,12 +61,12 @@ tasks {
     publish { dependsOn(provider { (getTasksByName("publish", true) - this).toList() }) }
 }
 
-fun org.ajoberstar.grgit.Commit.extractVersion(): String? {
-    val open = fullMessage.indexOf("[")
-    val close = fullMessage.indexOf("]")
-
-    if (open < 0 || close < 0) {
-        return null
-    }
-    return fullMessage.subSequence(open + 1, close).toString()
-}
+// fun org.ajoberstar.grgit.Commit.extractVersion(): String? {
+//     val open = fullMessage.indexOf("[")
+//     val close = fullMessage.indexOf("]")
+//
+//     if (open < 0 || close < 0) {
+//         return null
+//     }
+//     return fullMessage.subSequence(open + 1, close).toString()
+// }
