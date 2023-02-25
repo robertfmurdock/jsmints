56f71c78d445506aec211e5043992f583f5b4c42
RoB Murdock <robert.f.murdock@gmail.com>
2023-02-25 17:06:40 -0500

    [patch] removing additional caching pass, as it looks like caching is responsible for some of the weirdness seen recently with package.json version numbers in builds

--- jsmints-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/js.gradle.kts
+++ jsmints-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/js.gradle.kts
@@ -1,14 +1,6 @@
 package com.zegreatrob.jsmints.plugins
 
-import org.jetbrains.kotlin.gradle.plugin.mpp.MetadataDependencyTransformationTask
-import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrLink
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.npm.PublicPackageJsonTask
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmCachesSetup
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinPackageJsonTask
-import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest
-import org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest
-import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 
 plugins {
     kotlin("multiplatform")
@@ -44,35 +36,4 @@ rootProject.extensions.findByType(NodeJsRootExtension::class.java).let {
     if (it?.nodeVersion != "19.6.0") {
         it?.nodeVersion = "19.6.0"
     }
-}
-
-tasks.withType(PublicPackageJsonTask::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinNpmCachesSetup::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinPackageJsonTask::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinJsIrLink::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(Kotlin2JsCompile::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinJsTest::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(org.gradle.api.tasks.bundling.Jar::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(org.gradle.jvm.tasks.Jar::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinJvmTest::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(MetadataDependencyTransformationTask::class).configureEach {
-    outputs.cacheIf { true }
 }
