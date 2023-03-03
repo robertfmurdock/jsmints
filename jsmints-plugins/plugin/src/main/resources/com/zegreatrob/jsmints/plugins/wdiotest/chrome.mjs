1584a241ad199815ca566cd2756166fefe928bb3
RoB Murdock <robert.f.murdock@gmail.com>
2023-03-02 23:09:48 -0500

    [patch] adding small window size

--- jsmints-plugins/plugin/src/main/resources/com/zegreatrob/jsmints/plugins/wdiotest/chrome.mjs
+++ jsmints-plugins/plugin/src/main/resources/com/zegreatrob/jsmints/plugins/wdiotest/chrome.mjs
@@ -14,6 +14,7 @@ export function configure(config, directories) {
                 '--no-sandbox',
                 'headless',
                 'disable-dev-shm-usage',
+                'window-size=800,600',
             ]
         },
     })
