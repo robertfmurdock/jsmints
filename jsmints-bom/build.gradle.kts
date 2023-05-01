f53d6d4f7b25b1c51a2f4313f512a4082a475815
RoB Murdock <robert.f.murdock@gmail.com>
2023-05-01 14:46:01 -0400

    [minor] adding react testing library wrapper

--- jsmints-bom/build.gradle.kts
+++ jsmints-bom/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
         api(project(":minreact"))
         api(project(":minenzyme"))
         api(project(":react-data-loader"))
+        api(project(":react-testing-library"))
         api(project(":wdio"))
         api(project(":wdio-testing-library"))
         api("com.zegreatrob.jsmints.plugins.wdiotest:com.zegreatrob.jsmints.plugins.wdiotest.gradle.plugin")
