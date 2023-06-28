baa0b6b1880ab5c57a64933c149c24d7feef976d
RoB Murdock <robert.f.murdock@gmail.com>
2023-06-28 15:52:09 -0400

    [patch] restricting version regex to avoid -RC

--- jsmints-convention-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/versioning.gradle.kts
+++ jsmints-convention-plugins/src/main/kotlin/com/zegreatrob/jsmints/plugins/versioning.gradle.kts
@@ -19,7 +19,7 @@ tasks {
         revision = "release"
 
         rejectVersionIf {
-            "^[0-9.]+[0-9](-RC|-M[0-9]+|-RC[0-9]+|-beta.*|-alpha.*|-dev.*)\$"
+            "^[0-9.]+[0-9](-RC|-M[0-9]+|-RC[0-9]+|-beta.*|-alpha.*|-dev.*|-RC.*)\$"
                 .toRegex(RegexOption.IGNORE_CASE)
                 .matches(candidate.version)
         }
