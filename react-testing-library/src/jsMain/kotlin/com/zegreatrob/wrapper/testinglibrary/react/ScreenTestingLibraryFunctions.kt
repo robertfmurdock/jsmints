9b508c540d82ca23fbb42b615b27deb09969a7f2
RoB Murdock <robert.f.murdock@gmail.com>
2023-07-04 11:56:06 -0400

    adjusting new sugar to be easier to use

--- react-testing-library/src/jsMain/kotlin/com/zegreatrob/wrapper/testinglibrary/react/ScreenTestingLibraryFunctions.kt
+++ react-testing-library/src/jsMain/kotlin/com/zegreatrob/wrapper/testinglibrary/react/ScreenTestingLibraryFunctions.kt
@@ -2,9 +2,15 @@ package com.zegreatrob.wrapper.testinglibrary.react
 
 import com.zegreatrob.wrapper.testinglibrary.react.external.RenderOptions
 import com.zegreatrob.wrapper.testinglibrary.react.external.reactTestingLibrary
+import react.ChildrenBuilder
+import react.Fragment
 import react.ReactNode
+import react.create
 
 internal object ScreenTestingLibraryFunctions : TestingLibraryFunctions {
     override fun render(node: ReactNode) = reactTestingLibrary.render(node)
     override fun render(node: ReactNode, options: RenderOptions) = reactTestingLibrary.render(node, options)
+    override fun render(block: ChildrenBuilder.() -> Unit) = render(Fragment.create(block))
+    override fun render(options: RenderOptions, block: ChildrenBuilder.() -> Unit) =
+        render(Fragment.create(block), options)
 }
--- react-testing-library/src/jsMain/kotlin/com/zegreatrob/wrapper/testinglibrary/react/TestingLibraryFunctions.kt
+++ react-testing-library/src/jsMain/kotlin/com/zegreatrob/wrapper/testinglibrary/react/TestingLibraryFunctions.kt
@@ -3,15 +3,11 @@ package com.zegreatrob.wrapper.testinglibrary.react
 import com.zegreatrob.wrapper.testinglibrary.react.external.RenderOptions
 import com.zegreatrob.wrapper.testinglibrary.react.external.Result
 import react.ChildrenBuilder
-import react.Fragment
 import react.ReactNode
-import react.create
 
 interface TestingLibraryFunctions {
     fun render(node: ReactNode): Result
     fun render(node: ReactNode, options: RenderOptions): Result
+    fun render(block: ChildrenBuilder.() -> Unit): Result
+    fun render(options: RenderOptions, block: ChildrenBuilder.() -> Unit): Result
 }
-
-fun TestingLibraryFunctions.render(block: ChildrenBuilder.() -> Unit) = render(Fragment.create(block))
-fun TestingLibraryFunctions.render(options: RenderOptions, block: ChildrenBuilder.() -> Unit) =
-    render(Fragment.create(block), options)
