bd9c58653f8e75c973a4bd25d4f0a444851c8239
RoB Murdock <robert.f.murdock@gmail.com>
2023-07-04 11:15:43 -0400

    adding render convenience function for preferring builder function

--- react-testing-library/src/jsMain/kotlin/com/zegreatrob/wrapper/testinglibrary/react/TestingLibraryFunctions.kt
+++ react-testing-library/src/jsMain/kotlin/com/zegreatrob/wrapper/testinglibrary/react/TestingLibraryFunctions.kt
@@ -2,9 +2,16 @@ package com.zegreatrob.wrapper.testinglibrary.react
 
 import com.zegreatrob.wrapper.testinglibrary.react.external.RenderOptions
 import com.zegreatrob.wrapper.testinglibrary.react.external.Result
+import react.ChildrenBuilder
+import react.Fragment
 import react.ReactNode
+import react.create
 
 interface TestingLibraryFunctions {
     fun render(node: ReactNode): Result
     fun render(node: ReactNode, options: RenderOptions): Result
 }
+
+fun TestingLibraryFunctions.render(block: ChildrenBuilder.() -> Unit) = render(Fragment.create(block))
+fun TestingLibraryFunctions.render(options: RenderOptions, block: ChildrenBuilder.() -> Unit) =
+    render(Fragment.create(block), options)
