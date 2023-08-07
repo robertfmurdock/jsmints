922870e4bc05abaaa7d4cf5275aee245ef16dcd4
RoB Murdock <rob@zegreatrob.com>
2023-08-07 11:08:07 -0400

    fixing issues with kotlin wrappers, removing some unused minreact elements, and updating jsdom

--- libraries/minreact/src/jsMain/kotlin/com/zegreatrob/minreact/ReactFunction.kt
+++ libraries/minreact/src/jsMain/kotlin/com/zegreatrob/minreact/ReactFunction.kt
@@ -14,12 +14,6 @@ import react.create
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 
-inline fun <reified P : DataProps<P>> tmFC(crossinline function: ChildrenBuilder.(P) -> Unit): ElementType<DataPropsBridge> = FC { props: DataPropsBridge ->
-    val newProps = ensureKotlinClassProps(props, P::class.js)
-    +newProps.unsafeCast<Props>()
-    function(newProps)
-}
-
 inline fun <reified T : DataProps<T>> ntmFC(noinline function: ChildrenBuilder.(T) -> Unit) =
     NamedTmFC(T::class, function)
 
@@ -30,7 +24,7 @@ class NamedTmFC<T : DataProps<T>>(private val clazz: KClass<T>, private val func
     operator fun getValue(t: Any?, property: KProperty<*>): FC<DataPropsBridge> =
         fc ?: FC(property.name) { props: DataPropsBridge ->
             val newProps = ensureKotlinClassProps(props, clazz.js)
-            +newProps.unsafeCast<Props>()
+            objectAssign(this@FC, newProps)
             function(newProps)
         }.also {
             fc = it
@@ -66,8 +60,8 @@ typealias TMFC = ElementType<DataPropsBridge>
 
 val DataProps<*>.children get() = this.unsafeCast<PropsWithChildren>().children
 
-fun ChildrenBuilder.children(DataProps: DataProps<*>) {
-    Children.toArray(DataProps.children).forEach(::child)
+fun ChildrenBuilder.children(dataProps: DataProps<*>) {
+    Children.toArray(dataProps.children).forEach { +it }
 }
 
 fun <P : DataProps<P>> create(dataProps: DataProps<P>, block: @JsoDsl Props.() -> Unit = {}) =
