85f9c9ddaf9b09366b8bbf687213fcf45814e74d
RoB Murdock <rob@zegreatrob.com>
2024-07-11 15:41:19 -0400

    [minor] fixing breaking change in the kotlin react useEffect hook wrappers

--- libraries/react-data-loader/src/jsMain/kotlin/com/zegreatrob/react/dataloader/UseScope.kt
+++ libraries/react-data-loader/src/jsMain/kotlin/com/zegreatrob/react/dataloader/UseScope.kt
@@ -5,11 +5,11 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.plus
-import react.useEffectOnce
+import react.useEffectOnceWithCleanup
 import react.useState
 
 fun useScope(coroutineName: String): CoroutineScope {
     val (scope) = useState { MainScope() + CoroutineName(coroutineName) }
-    useEffectOnce { cleanup { scope.cancel() } }
+    useEffectOnceWithCleanup { onCleanup { scope.cancel() } }
     return scope
 }
