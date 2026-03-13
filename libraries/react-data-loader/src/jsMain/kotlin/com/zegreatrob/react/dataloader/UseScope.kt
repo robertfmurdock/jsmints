ed2805118f7e6ae87711097357d2f6c9af92add8
Rob Murdock <rob@zegreatrob.com>
2026-03-12 22:37:21 -0400

    Revert "[patch] adjusting usescope to wrappers change"
    This reverts commit dcc82e0877ad45895e6510f03e1c28b626038f7f.

--- libraries/react-data-loader/src/jsMain/kotlin/com/zegreatrob/react/dataloader/UseScope.kt
+++ libraries/react-data-loader/src/jsMain/kotlin/com/zegreatrob/react/dataloader/UseScope.kt
@@ -1,15 +1,17 @@
 package com.zegreatrob.react.dataloader
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.plus
 import react.useEffectOnce
 import react.useState
 
 fun useScope(coroutineName: String): CoroutineScope {
     val scope = useState { MainScope() + CoroutineName(coroutineName) }.component1()
     useEffectOnce {
-        this.coroutineContext.job.invokeOnCompletion {
-            scope.cancel()
-        }
+        awaitCleanup { scope.cancel() }
     }
     return scope
 }
