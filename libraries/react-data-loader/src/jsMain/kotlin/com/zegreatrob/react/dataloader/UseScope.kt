fad4a47579002705c9e50a5062ef066b56281754
Rob Murdock <rob@zegreatrob.com>
2026-03-13 08:29:13 -0400

    Revert "[patch] adjusting usescope to wrappers change"
    This reverts commit c668c36f349597a2d4fb4b373c9e169039eb7084.

--- libraries/react-data-loader/src/jsMain/kotlin/com/zegreatrob/react/dataloader/UseScope.kt
+++ libraries/react-data-loader/src/jsMain/kotlin/com/zegreatrob/react/dataloader/UseScope.kt
@@ -1,19 +1,17 @@
-@file:OptIn(ExperimentalWasmJsInterop::class)
-
 package com.zegreatrob.react.dataloader
 
-import com.zegreatrob.react.dataloader.react.useEffect
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.plus
+import react.useEffectOnce
 import react.useState
 
 fun useScope(coroutineName: String): CoroutineScope {
     val scope = useState { MainScope() + CoroutineName(coroutineName) }.component1()
-    useEffect({
-        { scope.cancel() }
-    }, dependencies = arrayOf())
+    useEffectOnce {
+        awaitCleanup { scope.cancel() }
+    }
     return scope
 }
--- libraries/react-data-loader/src/jsMain/kotlin/com/zegreatrob/react/dataloader/react/External.kt
+++
@@ -1,5 +0,0 @@
-@file:JsModule("react")
-
-package com.zegreatrob.react.dataloader.react
-
-internal external val useEffect: EffectHook
